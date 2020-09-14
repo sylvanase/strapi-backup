21e20389ecfb6b3295fa7cfbdf76be439185dd47
sylvanas <jzm362466464@126.com>
2020-09-14 23:59:32 +0800

    init strapi

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '738d66e3118ae50eb04fd5248d71fd64'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '46958fe7-54a9-44a1-bc38-103aa35baeb1'
+};
