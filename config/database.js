5f98418f872264574a9d6fd06c72c179406a2b9d
sylvanas <jzm362466464@126.com>
2020-09-15 17:42:50 +0800

    config database

--- config/database.js
+++ config/database.js
@@ -5,11 +5,12 @@ module.exports = ({ env }) => ({
       connector: 'bookshelf',
       settings: {
         client: 'mysql',
-        host: env('DATABASE_HOST', 'localhost'),// strapi部署的服务器与MySQL所在的服务器是同一个服务器，主机就直接写localhost
+        host: env('DATABASE_HOST', 'localhost'),
+        // strapi部署的服务器与MySQL所在的服务器是同一个服务器，主机就直接写localhost
         port: env.int('DATABASE_PORT', 3306),
         database: env('DATABASE_NAME', 'blog'),
         username: env('DATABASE_USERNAME', 'blog'),
-        password: env('DATABASE_PASSWORD', 'oPqc0Am9lfhWKNuT'),
+        password: env('DATABASE_PASSWORD', 'straPi DaTa0blog!'),
       },
       options: {},
     },
