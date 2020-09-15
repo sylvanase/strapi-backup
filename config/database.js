bcb75a234f0594d0f4f9a5db2dcbf50407ef7276
sylvanas <jzm362466464@126.com>
2020-09-15 22:45:54 +0800

    change pwd

--- config/database.js
+++ config/database.js
@@ -10,7 +10,7 @@ module.exports = ({ env }) => ({
         port: env.int('DATABASE_PORT', 3306),
         database: env('DATABASE_NAME', 'blog'),
         username: env('DATABASE_USERNAME', 'blog'),
-        password: env('DATABASE_PASSWORD', 'straPi DaTa0blog!'),
+        password: env('DATABASE_PASSWORD', 'straPiDaTa0blog!'),
       },
       options: {},
     },
